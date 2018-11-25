daf683d5b4b22e6729bec790beb7a1bd586b8a30
smdqromeo <1359247894@qq.com>
2018-11-25 16:25:46 +0800

    在main.js文件里面引用了element-ui并且在mounted用原生js设置了订单栏样式

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="pos">
-    <!-- <i class="icon iconfont icon-gongnengjianyi"></i>
-    <i class="icon iconfont icon-hanbao"></i>
-    <i class="icon iconfont icon-jisutuikuan"></i>
-
-    Hello AwesomePos Demo. -->
+   <el-row>
+     <el-col :span="7" class="pos-order" id="order-list">
+       我是订单栏
+     </el-col>
+     <el-col>
+       我是产品栏
+     </el-col>
+   </el-row>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Pos',
+  mounted:function(){
+var orderHeight = document.body.clientHeight;
+console.log(orderHeight);
+document.getElementById('order-list').style.height = orderHeight +'px'
+  },
   data () {
     return {
      
@@ -21,5 +29,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+ .pos-order{
+   background-color:#f9fafc;
+   border-right: 1px solid #c0ccda;
+ }
 </style>
