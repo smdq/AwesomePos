77b30629152c2cdb94c404108b8138de7f6c1309
smdqromeo <1359247894@qq.com>
2018-11-25 15:25:47 +0800

    在index文件里面通过link引入字体图标库的链接

---
+++ src/components/Pos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pos">
+    <i class="icon iconfont icon-gongnengjianyi"></i>
+    <i class="icon iconfont icon-hanbao"></i>
+    <i class="icon iconfont icon-jisutuikuan"></i>
+
+    Hello AwesomePos Demo.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pos',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
